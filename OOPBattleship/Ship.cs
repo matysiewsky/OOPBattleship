7c30a144a797c716903173081f878ce6583842a6
Maciej M <matysiewskimaciej@gmail.com>
2021-11-18 18:54:50 +0100

    Ship class - first try

--- OOPBattleship/Ship.cs
+++ OOPBattleship/Ship.cs
@@ -1,7 +1,17 @@
+using System;
+
 namespace OOPBattleship
 {
-    public class Ship
+    public class Ship: Square
     {
-        
+        public Ship(Tuple<int,int> coordinates, SquareStatus status): base(coordinates, status)
+        {
+
+        }
+
+        public Ship(Tuple<int, int>[,] coordinatesArray, SquareStatus status): base(coordinatesArray, status)
+        {
+
+        }
     }
 }
