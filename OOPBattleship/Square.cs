35df3ff4f70763ae546b5bc0e40012ab7454f499
Maciej M <matysiewskimaciej@gmail.com>
2021-11-18 18:54:28 +0100

    Implemented square class

--- OOPBattleship/Square.cs
+++ OOPBattleship/Square.cs
@@ -1,7 +1,53 @@
+using System;
+
 namespace OOPBattleship
 {
     public class Square
     {
-        
+        protected Tuple<int, int> positionCoordinates;
+
+        public SquareStatus status;
+
+        public Tuple<int, int> Position { get; }
+
+        public Square(Tuple<int, int> coordinates, SquareStatus status)
+        {
+            Position = coordinates;
+            this.status = status;
+        }
+
+        public Square BigShip(Tuple<int, int>[,] coordinatesArray, SquareStatus status)
+        {
+            // foreach (Tuple<int, int> coordinates in coordinatesArray)
+            // {
+            //     Po
+            // }
+        }
+
+        public string GetCharacter() => status switch
+        {
+            SquareStatus.Empty => "X",
+            SquareStatus.Ship => "Ship",
+            SquareStatus.Hit => "Hit",
+            SquareStatus.Missed => "Miss"
+        };
+
+        public override string ToString() => status switch
+        {
+            SquareStatus.Empty => "",
+            SquareStatus.Ship => "",
+            SquareStatus.Hit => "",
+            SquareStatus.Missed => ""
+        };
+
+    }
+
+
+    public enum SquareStatus
+    {
+        Empty = 0,
+        Ship = 1,
+        Hit = 2,
+        Missed = 3
     }
 }
