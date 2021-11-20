e72236baea327be1e2ac2a7da35f051bf8463787
Kamczata <kam.czernek@gmail.com>
2021-11-20 20:12:28 +0100

    started with board

--- OOPBattleship/Board.cs
+++ OOPBattleship/Board.cs
@@ -1,7 +1,39 @@
 namespace OOPBattleship
 {
-    public class Board
+    public class Board 
     {
+        public Square[,] ocean;
+        private bool _isPlacementOk;
+        private char[] alpha = " ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
 
+        bool IsPlacementOk 
+        { 
+            get { return _isPlacementOk; } 
+            set { _isPlacementOk = value; } 
+        }
+
+        public Board()
+        {
+            ocean = CreateBoard();
+        }
+
+        Square[,] CreateBoard()
+        {
+            Square[,] board = new Square[10,10];
+            for (int i= 0; i< board.Length; i ++)
+            {
+                for(int j=0; j< board.Length; j++)
+                {
+                    board[i, j] = new Square(i, j);
+                }
+            }
+            return board;
+        }
+
+        bool PlacementValidation()
+        {
+            return true;
+        }
+        
     }
 }
--- OOPBattleship/BoardFactory.cs
+++ OOPBattleship/BoardFactory.cs
@@ -1,7 +1,55 @@
+using System.Collections.Generic;
+
 namespace OOPBattleship
 {
-    public class BoardFactory
+    public class BoardFactory : Board
     {
-        
+        public List<string> takenSpots;
+        Dictionary<string, string> shipInfo = new Dictionary<string, string>();
+        public void RandomPlacement()
+        {
+            
+        }
+
+        public void ManualPlacement(Dictionary<string, string> shipInfo)
+        {
+            if (shipInfo["shipLength"] == "1")
+            {
+
+            }
+            else
+            {
+                if (shipInfo["position"] == "horizontal")
+                {
+                    if (shipInfo["shipLength"] == "2")
+                    {
+
+                    }
+                    else if (shipInfo["shipLength"] == "3")
+                    {
+
+                    } 
+
+                }
+                else if (shipInfo["position"] == "vertical")
+                {
+                    if (shipInfo["shipLength"] == "2")
+                    {
+
+                    }
+                    else if (shipInfo["shipLength"] == "3")
+                    {
+
+                    } 
+                }
+            }
+            private void lookForAdherentCells(int x, int y)
+            {
+
+            }
+
+            
+               
+        }
     }
 }
