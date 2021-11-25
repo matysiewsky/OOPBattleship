540edfb4eeb6b7e604a33937bbb93d1ae04a2120
Maciej M <matysiewskimaciej@gmail.com>
2021-11-25 19:22:19 +0100

    Enum fix

--- OOPBattleship/ShipInfo.cs
+++ OOPBattleship/ShipInfo.cs
@@ -6,20 +6,12 @@ namespace OOPBattleship
     {
         public enum ShipType
         {
-            Carrier,
-            Cruiser,
-            Battleship,
-            Submarine,
-            Destroyer
+            Carrier = 5,
+            Cruiser = 3,
+            Battleship = 4,
+            Submarine = 3,
+            Destroyer = 2
         }
 
-        public static readonly Dictionary<ShipType, int> ShipsSizes = new()
-        {
-            {ShipType.Carrier, 5},
-            {ShipType.Cruiser, 3},
-            {ShipType.Battleship, 4},
-            {ShipType.Submarine, 3},
-            {ShipType.Destroyer, 2}
-        };
     }
 }
