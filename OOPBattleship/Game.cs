cf831cd81ce63b64e94cae26946991d5e297e644
Maciej M <matysiewskimaciej@gmail.com>
2021-11-26 22:49:55 +0100

    created

--- OOPBattleship/Game.cs
+++ OOPBattleship/Game.cs
@@ -8,14 +8,25 @@ namespace OOPBattleship
 
         public Board board1;
         public Board board2;
+        public Player player1;
+        public Player player2;
         private List<Board> boards = new List<Board>();
+
+        private static List<Ship> shipsPlayer1 = new();
+        private static List<Ship> shipsPlayer2 = new();
+        private List<List<Ship>> shipsList = new()
+        {
+            shipsPlayer1,
+            shipsPlayer2
+        };
+
         private string placingPhase = "placing";
         private string shootingPhase = "shooting";
         public void Initialize(Display display, Input input, BoardFactory bf)
         {
             board1 = new Board(10);
             board2 = new Board(10);
-            
+
             boards.Add(board1);
             boards.Add(board2);
 
@@ -23,6 +34,7 @@ namespace OOPBattleship
 
             foreach (Board board in boards)
             {
+                foreach(List<Ship> list in shipsList)
                 foreach (ShipInfo.ShipType shipType in Enum.GetValues(typeof(ShipInfo.ShipType)))
                 {
                     Console.Clear();
@@ -32,14 +44,30 @@ namespace OOPBattleship
                     string shipDirection = input.ChooseVerticalOrHorizontal();
                     display.DisplayChoosingCoordinates();
                     Tuple<int, int> shipStartCoordinates = converter.ConvertShipCoordinates(input.GetShipPosition());
-                    bf.ManualPlacement(shipStartCoordinates, shipDirection, shipType, board);
+                    list.Add(bf.ManualPlacement(shipStartCoordinates, shipDirection, shipType, board));
+
+                }
+
+            }
+
+            Player player1 = new("Player 1", shipsPlayer1);
+            Player player2 = new("Player 2", shipsPlayer2);
+
+            Console.ReadLine();
+            
+        }
+
+        public void Round()
+        {
+            Player currentMove = player1;
+            while (!player1.IsPlayerAlive || !player2.IsPlayerAlive)
+            {
+                bool player1Turn = true;
+                while (player1Turn is true)
+                {
 
                 }
             }
-            
-            
-            Console.ReadLine();
-            
         }
     }
 }
